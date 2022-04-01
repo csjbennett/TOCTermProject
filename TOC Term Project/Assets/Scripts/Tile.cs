7419cc86534915e1c0371117f40bfcc38c77575e
csjbennett <60185886+csjbennett@users.noreply.github.com>
2022-03-31 21:29:54 -0400

    Initial work done for wang tile generation
    The infrastructure for generating tiles has been set in place, the script just needs a way to figure out which tile is suitable to place down in a given spot.

---
+++ TOC Term Project/Assets/Scripts/Tile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : ScriptableObject
+{
+    public int topColor;
+    public int bottomColor;
+    public int rightColor;
+    public int leftColor;
+
+    public Sprite sprite;
+}
---
+++ TOC Term Project/Assets/Scripts/Tile_Generator.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile_Generator : MonoBehaviour
+{
+    // Size of grid on the x axis
+    public int xSize;
+    // Size of grid on the y axis
+    public int ySize;
+    // Set of tiles to choose from
+    public Tile[] tileSet;
+    // Template tile that is instantiated into the scene
+    // and filled with data from tileSet
+    public GameObject templateTile;
+
+    private void Start()
+    {
+        GenerateTiles();
+    }
+
+    public void GenerateTiles()
+    {
+        int[,] grid = new int[xSize, ySize];
+        Tile[,] tiles = new Tile[xSize, ySize];
+
+        // 2D array for generating grid of tiles
+        for (int x = 0; x < grid.GetLength(0); x++)
+        {
+            for (int y = 0; y < grid.GetLength(1); y++)
+            {
+                // Sort through array of tiles until a suitable tile is found
+                if (x > 0 && y > 0)
+                {
+
+                }
+                // Randomly chooses the first tile
+                else
+                {
+                    var choice = tileSet[Random.Range(0, tileSet.GetLength(0))];
+
+                    tiles[x, y] = choice;
+
+                    var newTile = Instantiate(templateTile, new Vector3(x, y, 0), Quaternion.identity, null).GetComponent<Tile_Script>();
+                    
+                    newTile.topColor = choice.topColor;
+                    newTile.bottomColor = choice.bottomColor;
+                    newTile.rightColor = choice.rightColor;
+                    newTile.leftColor = choice.leftColor;
+
+                    newTile.spr.sprite = choice.sprite;
+                }
+            }
+        }
+    }
+}
