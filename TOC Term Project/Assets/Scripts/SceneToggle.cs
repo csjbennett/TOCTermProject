be14fb901d834c9c541994dc455f761bd91a471c
Brent Burtraw <brentburtraw@gmail.com>
2022-04-01 17:05:26 -0400

    UI / Fixed TileSet
    Nioce.

---
+++ TOC Term Project/Assets/Scripts/SceneToggle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneToggle : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
---
+++ TOC Term Project/Assets/TileDebug.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileDebug : MonoBehaviour
+{
+    public TextMesh[] textElements;
+
+    public void populateTextElements(int left = -1, int top = -1, int right = -1, int bottom = -1)
+    {
+        textElements[0].text = left.ToString();
+        textElements[1].text = top.ToString();
+        textElements[2].text = right.ToString();
+        textElements[3].text = bottom.ToString();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            foreach (TextMesh element in textElements)
+            {
+                element.gameObject.SetActive(!element.gameObject.activeSelf);
+            }
+        }
+    }
+}
